72a26b90be57c6c902b559cecbcca819ddb8dea3
Derek Nakamura <dereknakamura808@gmail.com>
2019-03-26 14:59:56 -0700

    Create test13.c

---
+++ phase6/tests/test13.c
@@ -0,0 +1,21 @@
+int main(void)
+{
+    int x, y, z;
+
+    x = 1;
+    y = 2;
+    z = 3;
+
+    if (y == z)
+	printf("oops!\n");
+
+    if (x < y)
+	printf("hello\n");
+    else
+	printf("there\n");
+
+    if (x > y)
+	printf("howdy\n");
+    else
+	printf("partner\n");
+}
